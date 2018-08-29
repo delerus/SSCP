5b0ade8f47b47adc288c273f68bceae1e252c8cb
Lars Erik Ødegaard <larserikodegaard@Larss-MacBook-Pro.local>
2018-08-29 09:37:33 -0700

    started with a shell for the darcy_model class

---
+++ darcymodel.py
@@ -0,0 +1,21 @@
+from fenics import *
+from mshr import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class darcy_model():
+    """
+    This class is gonna include the equations (Maths) and also the definitions of functionspaces etc.
+    """
+    def __init__(self):
+
+        pass
+
+    def fem_definitions():
+        pass
+    
+    def multi_comp_darcy_equation():
+        pass
+
+
