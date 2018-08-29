6dd1048eb9901ca4df39d5d27a1c88321d113e80
Lars Erik Ødegaard <larserikodegaard@Larss-MacBook-Pro.local>
2018-08-29 11:57:12 -0700

    created a method to set timesteps in the simulation

--- simulation.py
+++ simulation.py
@@ -8,8 +8,20 @@ Things i want this class to handle:
 
 """
 
+from darcymodel import darcy_model
+from scipy.interpolate import interp1d
+
 class Simulate():
 
-    def __init__(self):
-
-        pass
+    def __init__(self,model='perfusion',tstep = 50, ):
+        if model = 'perfusion':
+            self.mod = darcy_model()
+    
+    self.set_timesteps(t_steps)
+    
+    def set_timesteps():
+        """
+        Sets timesteps that will be run trough by the simulation
+        """
+        org_time = self.model.geo.timesteps
+        self.timesteps = np.linspace(org_time[0],org_time.max(),t_steps())
