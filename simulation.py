1f1b1df32759ccd2be7436565ea8d8a6851b2413
Lars Erik Ødegaard <larserikodegaard@Larss-MacBook-Pro.local>
2018-08-29 09:59:41 -0700

    Created a todo to the simulations module

--- simulation.py
+++ simulation.py
@@ -1,5 +1,12 @@
 "This module will take care of the simulations"
+"""
+Things i want this class to handle:
+    1. number of simulations:
+    2. Filesaving/naming
+        - Both creating file, saving file, making a description file of parameters, closing files
+    3. Solver
 
+"""
 
 class Simulate():
 
