d16c206134064cd7db56a6aba9d6b82d1be56857
Lars Erik Ødegaard <larserikodegaard@Larss-MacBook-Pro.local>
2018-08-29 11:34:13 -0700

    Added a source term to the equation

--- darcymodel.py
+++ darcymodel.py
@@ -36,7 +36,16 @@ class darcy_model():
         self.c1, self.c2, self.c3 = split(self.c)
         self.c1_n, self.c2_n, self.c3_n = split(self.c_n)
 
-    def multi_comp_darcy_equation():
+    def multi_comp_darcy_equation(self):
+        """
+        The simplified multi compartment dary equation
+
+        A1-3: Intracompartment perfusion in comp 1-3.
+        B1: Intercompartment perfusion between comp 1 and 2.
+        B2: Intercompartment perfusion between comp 2 and 3.
+
+        S: Sink term
+        """
         
         A1 = -self.geo.K1 * dot(grad(self.p1),grad(self.q1))*dx
         A2 = -self.geo.K2 * dot(grad(self.p2),grad(self.q2))*dx
@@ -45,6 +54,8 @@ class darcy_model():
         B1 = dot(self.geo.beta12*(p1-p2),q1)*dx + dot(self.geo.beta12*(p2-p1),q2)*dx
         B2 = dot(self.geo.beta23*(p2-p3),q2)*dx + dot(self.geo.beta23*(p3-p2),q3)*dx
 
-        self.F = A1+A2+A3+B1+B2
+        S = - Constant(0.1)*(p3-Constant(3.0))
+
+        self.F = A1+A2+A3+B1+B2+S
 
 
